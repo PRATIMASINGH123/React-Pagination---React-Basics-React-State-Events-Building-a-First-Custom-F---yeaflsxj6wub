a23841fe17b04b1593b018a0df0ff8d9216f10a0
PRATIMA <107994434+PRATIMASINGH123@users.noreply.github.com>
2022-09-30 12:27:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react'
 import '../styles/App.css';
-import Loader from './Loader';
-import PostList from './PostList';
-// import fetchPosts from '../api/fetchPosts';
-import PaginationButtonsList from './PaginationButtonsList';
+import { PostList } from './PostList';
 const App = () => {
-    const [posts, setPosts] = useState([]);
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(true);
 
-    const handlePage = (pageNumber) => {
-      setPage(pageNumber);
-    }
-
-  useEffect(() => {
-    const getData = async (page, limit) => {
-      setLoading(true);
-      const data = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`)
-      const res = await data.json()
-      setPosts(res);
-      setLoading(false)
-    }
-    getData(page, 5);
-  }, [page])
-  
   return (
     <div id="main">
-      
-      {loading ? <Loader /> : <PostList posts= {posts}/>}
-      <PaginationButtonsList page={page} handlePage={handlePage}/>
+      <PostList />
     </div>
   )
 }
